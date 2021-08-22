644ccdc9904c87df6bb385e6116882a259ff662b
Vinod Garg <vinodgarg@Vinods-MacBook-Pro.local>
2021-08-22 11:26:16 -0700

    Added constants class for all hard coded values

--- src/main/java/com.adobe.aem/util/RomanNumeralValidations.java
+++ src/main/java/com.adobe.aem/util/RomanNumeralValidations.java
@@ -1,13 +1,14 @@
 package com.adobe.aem.util;
 
 import org.springframework.stereotype.Component;
+import static com.adobe.aem.util.RomanNumeralConstants.INVALID_QUERY_ERROR_MESSAGE;
 
 @Component
 public class RomanNumeralValidations {
 
     public boolean validateQueryValue(int num) {
         if (num <= 0 || num > 255) {
-            throw new IllegalArgumentException(RomanNumeralConstants.INVALID_QUERY_ERROR_MESSAGE);
+            throw new IllegalArgumentException(INVALID_QUERY_ERROR_MESSAGE);
         }
         return true;
     }
