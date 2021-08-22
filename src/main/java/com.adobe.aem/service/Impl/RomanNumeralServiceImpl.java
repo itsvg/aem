70fc9e727fb123aa038a8f99ae05e948b6ed86fc
Vinod Garg <vinodgarg@Vinods-MacBook-Pro.local>
2021-08-22 11:23:17 -0700

    Added constants class for all hard coded values

--- src/main/java/com.adobe.aem/service/Impl/RomanNumeralServiceImpl.java
+++ src/main/java/com.adobe.aem/service/Impl/RomanNumeralServiceImpl.java
@@ -7,17 +7,17 @@ import org.springframework.stereotype.Component;
 public class RomanNumeralServiceImpl implements RomanNumeralService {
 
     /**
-     * Integer value.
+     * Integer values list.
      */
-    private static int[] values = {100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static int[] INT_VALUES = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
     /**
-     * Roman numerals.
+     * Roman numerals mapping list.
      */
-    private static String[] roman = {"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    private static String[] ROMAN_NUMERALS = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
     /**
-     * Get Roman Numeral equivalent to integer value in range {1-255}.
+     * Get Roman Numeral equivalent to integer value in range {1-3999}.
      *
      * @param num
      * @return
@@ -26,10 +26,10 @@ public class RomanNumeralServiceImpl implements RomanNumeralService {
 
         StringBuilder result = new StringBuilder();
         while (num > 0) {
-            for (int i = 0; i < values.length; i++) {
-                if (num >= values[i]) {
-                    result.append(roman[i]);
-                    num -= values[i];
+            for (int i = 0; i < INT_VALUES.length; i++) {
+                if (num >= INT_VALUES[i]) {
+                    result.append(ROMAN_NUMERALS[i]);
+                    num -= INT_VALUES[i];
                     break;
                 }
             }
--- src/main/java/com.adobe.aem/util/RomanNumeralValidations.java
+++ src/main/java/com.adobe.aem/util/RomanNumeralValidations.java
@@ -7,7 +7,7 @@ public class RomanNumeralValidations {
 
     public boolean validateQueryValue(int num) {
         if (num <= 0 || num > 255) {
-            throw new IllegalArgumentException("Invalid query value");
+            throw new IllegalArgumentException(RomanNumeralConstants.INVALID_QUERY_ERROR_MESSAGE);
         }
         return true;
     }
